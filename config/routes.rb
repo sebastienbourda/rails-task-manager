47f12a4e07312c944c938d698b7d4bc82c447906
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2022-08-16 13:06:26 +0200

    add update method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/", to: "tasks#index"
-  get "/:id", to: "tasks#show"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: 'task'
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
